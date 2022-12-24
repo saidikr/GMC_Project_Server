7a8091999159cb56f1bc0c4004ad3c5f6c77ef1b
said ikrouberkane <said.ikrouberkane@gmail.com>
2022-12-24 20:39:17 +0100

    last changess

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-const { MONGO_DEV_URL } = process.env;
+const { MONGO_PROD_URL } = process.env;
 exports.connect = () => {
   mongoose
-    .connect(MONGO_DEV_URL, {
+    .connect(MONGO_PROD_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
